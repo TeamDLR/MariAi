4e4adfc6242c77f7ef8b48154ab055c541210bf5
Dag7 <dag7@protonmail.com>
2024-06-07 16:26:37 +0200

    feat: multiple demo all in one

--- main.py
+++ main.py
@@ -19,18 +19,11 @@ except FileNotFoundError:
         "\n[WARNING] No secrets.env file found. Please create one with the required keys. External APIs will not work.\n"
     )
 
-
-def do_nothing():
-    print("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-
-
-def real_action_recognition():
+def real_action_recognition(video_file="https://archive.org/serve/ISSVideoResourceLifeOnStation720p/ISS%20Video%20Resource_LifeOnStation_720p.mp4"):
     USE_WEBCAM = False
     cam_id = 0
-    # video_file_2 = "https://archive.org/serve/ISSVideoResourceLifeOnStation720p/ISS%20Video%20Resource_LifeOnStation_720p.mp4"
-    # video_file_3 = "https://ia801304.us.archive.org/13/items/bctvnj-Cooking_Eggs_for_Family/Cooking_Eggs_for_Family.mpeg4"
-    # video_file_4 = "https://dn720404.ca.archive.org/0/items/80-year-old-man-chopping-wood/80%20Year%20Old%20Man%20Chopping%20Wood.mp4"
-    video_file = "https://ia600609.us.archive.org/26/items/Game_of_the_Week_Play_of_the_Game_-_Charles_Alexander_Block_Basket_and_1/Game_of_the_Week_Play_of_the_Game_-_Charles_Alexander_Block_Basket_and_1.mp4"
+    if not video_file:
+        return
     source = cam_id if USE_WEBCAM else video_file
     additional_options = (
         {"skip_first_frames": 600, "flip": False} if not USE_WEBCAM else {"flip": True}
@@ -57,7 +50,24 @@ def real_chatbot():
     )
 
 
-def demo_all_in_one():
+
+def demo_1():
+    video_file = "https://ia600609.us.archive.org/26/items/Game_of_the_Week_Play_of_the_Game_-_Charles_Alexander_Block_Basket_and_1/Game_of_the_Week_Play_of_the_Game_-_Charles_Alexander_Block_Basket_and_1.mp4"
+
+    demo_all_in_one(video_file)
+
+def demo_2():
+    video_file = "https://ia801304.us.archive.org/13/items/bctvnj-Cooking_Eggs_for_Family/Cooking_Eggs_for_Family.mpeg4"
+
+    demo_all_in_one(video_file)
+
+def demo_3():
+    video_file = "https://archive.org/serve/ISSVideoResourceLifeOnStation720p/ISS%20Video%20Resource_LifeOnStation_720p.mp4"
+    demo_all_in_one(video_file)
+
+
+
+def demo_all_in_one(video_file):
     # Step 0: STT
     text = real_speech_to_text(language_code="it-IT").lower()
 
@@ -67,7 +77,7 @@ def demo_all_in_one():
     print(f"What you said: {text}")
 
     # Step 1: Show the video and recognize action
-    action = real_action_recognition()
+    action = real_action_recognition(video_file)
     print(f"Action recognized: {action}")
 
     # Step 2: Use the action to form a question
@@ -106,13 +116,16 @@ button.grid(row=0, column=2, sticky="news", padx=20, pady=10)
 button = tkinter.Button(frame, text="Speech To Text", command=real_speech_to_text)
 button.grid(row=0, column=4, sticky="news", padx=20, pady=10)
 
-button = tkinter.Button(frame, text="Show Graphs", command=do_nothing)
-button.grid(row=0, column=6, sticky="news", padx=20, pady=10)
-
 button = tkinter.Button(frame, text="Chatbot", command=real_chatbot)
-button.grid(row=0, column=8, sticky="news", padx=20, pady=12)
+button.grid(row=0, column=6, sticky="news", padx=20, pady=12)
 
-button = tkinter.Button(frame, text="Demo All-in-One", command=demo_all_in_one)
-button.grid(row=0, column=10, sticky="news", padx=20, pady=12)
+button = tkinter.Button(frame, text="Demo All-in-One 1", command=demo_1)
+button.grid(row=1, column=0, sticky="news", padx=20, pady=12)
+
+button = tkinter.Button(frame, text="Demo All-in-One 2", command=demo_2)
+button.grid(row=1, column=2, sticky="news", padx=20, pady=12)
+
+button = tkinter.Button(frame, text="Demo All-in-One 3", command=demo_3)
+button.grid(row=1, column=4, sticky="news", padx=20, pady=12)
 
 window.mainloop()
